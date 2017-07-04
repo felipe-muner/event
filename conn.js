0ecfda5f62e8ebed434898671d9c9477c63ec6dc
Felipe Muner <felipe.muner@gmail.com>
2017-07-04 20:03:52 -0300

    First commit to clean view

--- conn.js
+++ conn.js
@@ -9,7 +9,8 @@ function Connection() {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
-      database: process.env.DB_BASE
+      database: process.env.DB_BASE,
+      dateStrings: 'date'
     });
   };
 
