ba9f69869c5aeadd76c8be189f08077ec9e0e56d
Felipe Muner British <felipe.muner@gmail.com>
2017-07-17 16:46:20 -0300

    Starting external event

--- model/ExternalEvent.js
+++ model/ExternalEvent.js
@@ -2,61 +2,7 @@ const conn = require(process.env.PWD + '/conn');
 const moment = require('moment')
 const fs = require('fs');
 
-function InternalEvent(){
-  this.getAllSite = function(req, res, next){
-    conn.acquire(function(err,con){
-      con.query('SELECT idunidade, unidade FROM unidades', function(err, result) {
-        con.release();
-        if(err){
-          res.render('error', { error: err } );
-        }else{
-          req.allSite = result
-          next()
-        }
-      });
-    });
-  }
-  this.getAllBuilding = function(req, res, next){
-    conn.acquire(function(err,con){
-      con.query('Select BuildingID, Site_ID, Name from Building WHERE Status = 1', function(err, result) {
-        con.release();
-        if(err){
-          res.render('error', { error: err } );
-        }else{
-          req.allBuilding = result
-          next()
-        }
-      });
-    });
-  }
-  this.getAllRoom = function(req, res, next){
-    conn.acquire(function(err,con){
-      con.query('Select RoomID, Building_ID, Name, Status FROM Room Where Status = 1 AND CanEvent = 1', function(err, result) {
-        con.release();
-        if(err){
-          res.render('error', { error: err } );
-        }else{
-          console.log(req.allSite);
-          req.allRoom = result
-          next()
-        }
-      });
-    });
-  }
-
-  // this.searchEvents = function(req, res, next){
-  //   conn.acquire(function(err,con){
-  //     con.query('SELECT EventID, Name, StartingDate, StartingTime, EndTime FROM Event WHERE YEAR(StartingDate) = ? AND MONTH(StartingDate) = ?', [req.body.currentYear, req.body.currentMonth],function(err, result) {
-  //       con.release();
-  //       if(err){
-  //         res.render('error', { error: err } );
-  //       }else{
-  //         req.allEvents = result
-  //         next()
-  //       }
-  //     });
-  //   });
-  // }
+function ExternalEvent(){
 
   this.searchEventTwoDate = function(req, res, next){
     conn.acquire(function(err,con){
@@ -84,8 +30,7 @@ function InternalEvent(){
                 'WHERE '+
                   'e.Type = ? AND '+
                   'Date(StartEvent) >= ? AND '+
-                  'Date(StartEvent) < ? AND '+
-                  'e.Room_ID =  ?', ['I', req.body.firstDay, req.body.lastDay, req.body.roomID],function(err, result) {
+                  'Date(StartEvent) < ?', ['E', req.body.firstDay, req.body.lastDay],function(err, result) {
         con.release();
         console.log(this.sql);
         if(err){
@@ -143,37 +88,6 @@ function InternalEvent(){
     });
   }
 
-  this.getAllSiteBuildingRoom = function(req, res, next){
-    conn.acquire(function(err,con){
-      con.query('SELECT '+
-                  'u.idunidade, '+
-                  'upper(u.unidade) as unidade, '+
-                  'b.BuildingID, '+
-                  'b.Name as NameBuilding, '+
-                  'r.RoomID, '+
-                  'r.Name as NameRoom '+
-                'FROM '+
-                  'Room AS r '+
-                  'Inner Join Building AS b ON r.Building_ID = b.BuildingID '+
-                  'Inner Join unidades AS u ON b.Site_ID = u.idunidade '+
-                'WHERE '+
-                  'r.Status = 1 '+
-                'ORDER BY '+
-                  'u.idunidade ASC, '+
-                  'b.BuildingID ASC, '+
-                  'r.RoomID ASC', function(err, result) {
-        con.release();
-        if(err){
-          res.render('error', { error: err } );
-        }else{
-          // console.log(this.sql);
-          req.getAllSiteBuildingRoom = result
-          next()
-        }
-      });
-    });
-  }
-
   this.getLastEvent = function(req, res, next){
     console.log(req.body);
     conn.acquire(function(err,con){
@@ -202,7 +116,7 @@ function InternalEvent(){
     let EndEvent = moment(req.body.dateNewEvent + 'T' + req.body.endTimeNewEvent).format('YYYY-MM-DD HH:mm:ss')
 
     let event = {
-      Type:'I',
+      Type:'E',
       EventCode:req.nextEventCode,
       StartEvent,
       EndEvent,
@@ -252,4 +166,4 @@ function InternalEvent(){
 
 }
 
-module.exports = new InternalEvent()
+module.exports = new ExternalEvent()
--- routes/ExternalEvent.js
+++ routes/ExternalEvent.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const conn = require(process.env.PWD + '/conn');
 const Util = require(process.env.PWD + '/util/Util')
 const mailSender = require(process.env.PWD + '/util/MailSender')
+const mi = require(process.env.PWD + '/model/MenuItem')
+const d = require(process.env.PWD + '/model/Departament')
+const ee = require(process.env.PWD + '/model/ExternalEvent')
+const g = require(process.env.PWD + '/model/Guest')
+const u = require(process.env.PWD + '/model/User')
 const fs = require('fs');
 const moment = require('moment');
 const md5 = require('md5');
@@ -12,6 +17,9 @@ const A4option = require(process.env.PWD + '/views/report/A4config')
 router.get('/', function(req, res, next) {
   console.log('entrei evento routa externo');
   res.render('external-event/external-event',{dados:'felipe externo',sess:req.session})
-});
+}).post('/search-events', ee.searchEventTwoDate, function(req, res, next) {
+  //console.log(req.allEvents);
+  res.json(req.allEvents)
+})
 
 module.exports = router;
