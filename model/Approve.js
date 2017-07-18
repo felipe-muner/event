2e03d47004a5a6b4ba74e5e77053ec5f4d80db31
Felipe Muner British <felipe.muner@gmail.com>
2017-07-18 17:05:58 -0300

    Adding approve, checkbox and send to route to update in model module

--- model/Approve.js
+++ model/Approve.js
@@ -1,19 +1,39 @@
 const conn = require(process.env.PWD + '/conn');
 
 function Approve(){
-  this.all = function(req, res, next){
+
+  this.getEventToApprove = function(req, res, next){
     conn.acquire(function(err,con){
-      con.query('SELECT , nomedepartamento FROM departamentos ORDER by nomedepartamento', function(err, result) {
+      con.query('SELECT '+
+                  'e.EventCode, '+
+                  'e.Type, '+
+                  'e.CreateBy, '+
+                  'e.ResponsibleByEvent, '+
+                  'e.Name AS title, '+
+                  'e.StartEvent AS start, '+
+                  'e.EndEvent AS end, '+
+                  'es.StatusName, '+
+                  'e.DepartureFrom, '+
+                  'u2.nomeusuario AS ResponsibleByName, '+
+                  'u1.nomeusuario AS CreatedByName '+
+                'FROM '+
+                  'Event AS e '+
+                  'Inner Join EventStatus AS es ON e.EventStatus_ID = es.EventStatusID '+
+                  'Inner Join usuarios AS u1 ON e.CreateBy = u1.matricula '+
+                  'Left Join usuarios AS u2 ON u2.matricula = e.ResponsibleByEvent '+
+                'WHERE '+
+                  'e.EventStatus_ID = 1', [], function(err, result) {
         con.release();
         if(err){
           res.render('error', { error: err } );
         }else{
-          req.allDepartament = result
+          req.allEventToApprove = result
           next()
         }
       });
     });
   }
+
   this.aproveEvent = function(req, res, next){
     conn.acquire(function(err,con){
       con.query('UPDATE Event SET EventStatus_ID = ?, ApprovedAt = NOW(), ApprovedBy = ? WHERE EventCode = ?', [,], function(err, result) {
@@ -21,7 +41,7 @@ function Approve(){
         if(err){
           res.render('error', { error: err } );
         }else{
-          req.allDepartament = result
+          req.allEventAvaliados = result
           next()
         }
       });
--- routes/Approve.js
+++ routes/Approve.js
@@ -3,15 +3,31 @@ const router = express.Router();
 const conn = require(process.env.PWD + '/conn');
 const Util = require(process.env.PWD + '/util/Util')
 const mailSender = require(process.env.PWD + '/util/MailSender')
+const approve = require(process.env.PWD + '/model/Approve')
 const fs = require('fs');
 const moment = require('moment');
 const md5 = require('md5');
 const pdf = require('html-pdf');
 const A4option = require(process.env.PWD + '/views/report/A4config')
 
-router.get('/', function(req, res, next) {
-  console.log('vou aprovar');
-  res.render('approve/approve',{dados:'felipe approver',sess:req.session})
-});
+router.get('/', approve.getEventToApprove ,function(req, res, next) {
+
+  req.allEventToApprove.map(e => {
+    e.Type === 'I' ? e.Type = 'Internal' : e.Type = 'External'
+    e.ResponsibleByName = Util.toTitleCase(e.ResponsibleByName)
+    e.CreatedByName = Util.toTitleCase(e.CreatedByName)
+    e.start = moment(e.start).format('DD/MM/YYYY HH:mm')
+    e.end = moment(e.end).format('DD/MM/YYYY HH:mm')
+    e.title = Util.toTitleCase(e.title)
+  })
+
+  res.render('approve/approve', {
+    sess:req.session,
+    allEventToApprove:req.allEventToApprove
+  })
+}).post('/approve-event', function(req, res, next) {
+  console.log(req.body);
+  res.redirect('/approve')
+})
 
 module.exports = router;
